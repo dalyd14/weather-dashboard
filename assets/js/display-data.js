cb3dffed1208a5fc5ee50ce04e2006d69da3768c
David Daly <dalyd14@gmail.com>
2020-11-17 17:19:22 -0500

    connected to 5 day forecast data

--- assets/js/display-data.js
+++ assets/js/display-data.js
@@ -1,8 +1,26 @@
-var populatePage = function(data) {
-    $("#searched-location").text(data.name)
+var populateLocation = function(data) {
+    $("[id=searched-location]").text(data.name)
+}
+var populateCurrent = function(forecastData) {
     $("#todays-date").text(moment().format("MM/DD/YYYY"))
-    $("#current-temp").text(data.main.temp + " F")
-    $("#current-humidity").text(data.main.humidity + "%")
-    $("#current-wind-speed").text(data.wind.speed + " mph")
-    $("#current-uv-index").text("Not Available")
+    $("#current-temp").text(forecastData.current.temp + " F")
+    $("#current-humidity").text(forecastData.current.humidity + "%")
+    $("#current-wind-speed").text(forecastData.current.wind_speed + " mph")
+    $("#current-uv-index").text(forecastData.current.uvi)
+}
+// var populateForecast = function(forecastData) {
+//     $("#todays-date").text(moment().format("MM/DD/YYYY"))
+//     $("#current-temp").text(forecastData.current.temp + " F")
+//     $("#current-humidity").text(forecastData.current.humidity + "%")
+//     $("#current-wind-speed").text(forecastData.current.wind_speed + " mph")
+//     $("#current-uv-index").text(forecastData.current.uvi)
+// }
+var domForecastElGeneration = function(data) {
+    for(var i=1; i < 6; i++) {
+        var dayDiv = $("<div>").addClass("day")
+        var dayDate = $("<h5>").text(moment.unix(data.daily[i].dt).utc().format("MM/DD/YYYY"))
+        var dayTemp = $("<p>").text("Temperature: " + data.daily[i].temp.day + " F")
+        dayDiv.append(dayDate, dayTemp)
+        $("#forecast-container").append(dayDiv)
+    }
 }
